5dcdfcb12f6f4a3af735f91e9c96a7b75e798182
kanenatalia <n_ermolaeva@yahoo.com>
2018-12-21 21:03:33 -0800

    Removed comments

--- test/test.js
+++ test/test.js
@@ -3,20 +3,20 @@ const assert = require('assert');
 describe('Page opening', function () {
 
     it('get title', function(){
-        browser.url('/'); //open baseUrl + string passed in .url() function
-        let title = browser.getTitle(); //get page title and assign it to the "title" variable
-        assert.equal(title, 'Bug Tracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+        browser.url('/');
+        let title = browser.getTitle();
+        assert.equal(title, 'Bug Tracker', 'Title is incorrect');
     })
 
     it('get header title', function(){
         let title = browser.getText('.custom-header span');
-        assert.equal(title, 'BugTracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+        assert.equal(title, 'BugTracker', 'Title is incorrect');
     })
 
     it('click registration button', function(){
         browser.click('#registration');
         let result = browser.waitForVisible('button=< Back', 5000);
-        assert.equal(result, true, 'Button is not clicked'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+        assert.equal(result, true, 'Button is not clicked');
     })
 
 });
